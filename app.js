4e1fd77df64e99153d6972dac2cd6e569eb5f054
Manan Hora <horamanan@gmail.com>
2015-09-05 10:17:25 -0400

    send data to Twilio

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var emailList; //to store emails from last ten days and send data to Twilio
 
-var email = function (replyTime, subject, name){
-
+var email = function(replyTime, subject, name) {
+    this.replyTime = replyTime;
+    this.subject = subject;
+    this.name = name;
 };
+
+function sendDataTwilio(){
+	for(var i = 0; i<emailList.length;i++){
+		email = emailList[i];
+		if(email.replyTime!=null){
+			//send get request to Twilio..
+		}
+	}
+};
+
+
